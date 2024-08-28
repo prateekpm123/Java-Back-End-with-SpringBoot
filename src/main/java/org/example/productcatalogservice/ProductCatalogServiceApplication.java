560645a770ffdf3924a351d7fad524aac783f54e
Prateek <prateekpm123@gmail.com>
2024-08-28 15:37:15 +0530

    Connected database to Spring Hibernet

--- src/main/java/org/example/productcatalogservice/ProductCatalogServiceApplication.java
+++ src/main/java/org/example/productcatalogservice/ProductCatalogServiceApplication.java
@@ -8,6 +8,7 @@ public class ProductCatalogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
+
     }
 
 }
